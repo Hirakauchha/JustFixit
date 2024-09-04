e7da39b9c640167218a7e9bfc1420d406f5c122d
hira_kauchha <hirakauchha1234@gmail.com>
2024-09-04 11:55:23 +1000

    created user update api

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,3 +1,5 @@
+const UserModel = require("../models/user.model");
+
 const getCurrentUserData=async(req,res)=> {
     try {
         const user= req.user;
@@ -9,6 +11,35 @@ const getCurrentUserData=async(req,res)=> {
     }
 
 }
+const updateCurrentUserData=async(req,res)=>{
+    try {
+        const user_id=req.user._id;
+        const updatedUser= await UserModel.findByIdAndUpdate(
+            user_id,
+            {
+                fullname:req.body.fullname,
+                email:req.body.email,
+                address:req.body.address,
+            },{
+                new: true,
+                runValidators:true,
+            }
+        );
+        console.log(req.body);
+
+        if(!updatedUser){
+            return res.status(404).json({message:"User not found"});
+        }
+        res.status(200).json({
+            message:'User updated successfully',
+            user:updatedUser
+        })
+    } catch (error) {
+        res.status(500).json({message:error.message});
+        
+    }
+}
 module.exports={
-    getCurrentUserData
+    getCurrentUserData,
+    updateCurrentUserData
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,9 @@
 const {Router}=require('express');
 const router=Router();
-const {getCurrentUserData}=require('../controllers/usercontroller.js');
+const {getCurrentUserData,updateCurrentUserData}=require('../controllers/usercontroller.js');
 const {protectRoute,restrictTo}= require('../controllers/auth.controller.js');
 
 
 router.get('/currentuserdata',protectRoute,getCurrentUserData);
+router.patch('/updatecurrentuser',protectRoute,updateCurrentUserData);
 module.exports=router;
